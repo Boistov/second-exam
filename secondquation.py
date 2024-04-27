65852ebe0f48b309cfd91f541278447d5f08575c
Saidakbar Boistov <150612406+Boistov@users.noreply.github.com>
2024-04-27 19:08:48 +0400

    Update secondquation.py

--- secondquation.py
+++ secondquation.py
@@ -1,12 +1,6 @@
-a = int(input().strip())
-
-i = []
-for num in range(1, a):
-    m = num ** 2
-    if n <= a:
-        i.append(n)
-    else:
-        break
-
-print(*i)
+n=int(input())
+for i in range(n):
+    if i*i<n:
+        print(i*i)
+        
 
